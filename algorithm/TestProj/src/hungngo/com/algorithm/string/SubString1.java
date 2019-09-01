d38e83336d94906b7d7cb48fc041874c3addee27
hungngo0309 <39835798+hungngo0309@users.noreply.github.com>
2019-08-31 22:18:40 -0700

    add Substring1

---
+++ algorithm/TestProj/src/hungngo/com/algorithm/string/SubString1.java
@@ -0,0 +1,74 @@
+package hungngo.com.algorithm.string;
+
+public class SubString1 {
+
+	public SubString1() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getMinSubStringLenght(String s, String t) {
+		int len = -1;
+		
+		StringBuffer tmpStr = new StringBuffer();
+		for (int i=0; i<t.length(); i++) {
+			tmpStr.append(t.charAt(i));
+			
+			if (checkConcatenated(s, t, tmpStr.toString())) {
+				len = tmpStr.length();
+				break;
+			} else {
+				
+			}
+		}
+		
+		return len;
+	}
+	
+	private boolean checkConcatenated(String s, String t, String subString) {
+		boolean res = false;
+		
+		String checkedStr = subString;
+		while (checkedStr.length() <= t.length()) {
+			if (checkedStr.equals(t)) {
+				res = true;
+				break;
+			} else {
+				checkedStr = checkedStr.concat(checkedStr);
+			}
+		}
+		
+		if (res) {
+			checkedStr = subString;
+			while (checkedStr.length() <= s.length()) {
+				if (checkedStr.equals(s)) {
+					res = true;
+					break;
+				} else {
+					checkedStr = checkedStr.concat(checkedStr);
+				}
+			}
+		}
+		
+		return res;
+	}
+
+	public static void main(String[] args) {
+		/*
+		 ababab
+ 		 abab	
+		   -> 2
+		
+		 abcd
+		 abcd
+		   -> 4
+		*/
+		
+		// TODO
+		String s = "abcd";
+		String t = "abcd";
+		
+		SubString1 sub = new SubString1();
+		System.out.print("lenght of min substring= " +sub.getMinSubStringLenght(s, t)); 
+	}
+
+}
